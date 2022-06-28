04b686358e93dedbf21d872d4ff4a8fb32a51b0e
Yadan Wei <yadanwei0712@gmail.com>
2022-06-28 15:22:24 -0700

    "Checkout and payment complete"

---
+++ orders/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('place_order/', views.place_order, name='place_order'),
+    path('payments/', views.payments, name='payments'),
+    path('order_complete', views.order_complete, name='order_complete')
+]
